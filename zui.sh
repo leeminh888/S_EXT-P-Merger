4ed7e79585787cc08a4faa59566bb82845f27d09
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2020-11-15 13:19:40 -0300

    [UP] Bump to v1.2 If you want merge ZUI firmware, you need extract: super_2, super_3 and super_5 files from firmware and run zui.sh (with the extracted files in the same folder as the zui.sh file), and wait for merge. Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

--- zui.sh
+++ zui.sh
@@ -21,23 +21,15 @@ SYSTEM_NEW_IMAGE="$LOCALDIR/system_new.img"
 
 ## Mount Point vars for system
 SYSTEM="$LOCALDIR/system"
-SYSTEM_IMAGE="$LOCALDIR/system.img"
+SYSTEM_IMAGE="$LOCALDIR/super_2.img"
 
 ## Mount Point vars for product
 PRODUCT="$LOCALDIR/product"
-PRODUCT_IMAGE="$LOCALDIR/product.img"
+PRODUCT_IMAGE="$LOCALDIR/super_3.img"
 
 ## Mount Point vars for odm
 ODM="$LOCALDIR/odm"
-ODM_IMAGE="$LOCALDIR/odm.img"
-
-## Mount Point vars for opproduct
-OPPRODUCT="$LOCALDIR/opproduct"
-OPPRODUCT_IMAGE="$LOCALDIR/opproduct.img"
-
-## Mount Point vars for system_ext
-SYSTEM_EXT="$LOCALDIR/system_ext"
-SYSTEM_EXT_IMAGE="$LOCALDIR/system_ext.img"
+ODM_IMAGE="$LOCALDIR/super_5.img"
 
 CREDITS() {
    # Just a comment in build.prop
@@ -49,27 +41,8 @@ CREDITS() {
    echo "" >> build.prop
 }
 
-usage() {
-    echo "Usage: $0 <Path to firmware>"
-    echo -e "\tPath to firmware: the zip!"
-}
-
-if [ ! -f "$FE/extractor.sh" ]; then
-   echo "-> Firmware Extractor isn't cloned or don't exists! Exit 1."
-   exit 1
-else
-   if [ "$1" == "" ]; then
-      echo "-> Enter all needed parameters."
-      usage
-      exit 1
-   fi
-   echo "-> Starting the process..."
-   cd $FE; chmod +x -R *
-   bash $FE/extractor.sh "$1" "$LOCALDIR"
-fi
-
-# system.img
-echo "-> Check mount/etc for system.img"
+# super_2 (system)
+echo "-> Check mount/etc for super_2 (system)"
 if [ -f "$SYSTEM_IMAGE" ]; then
    # Check for AB/Aonly in system
    if [ -d "$SYSTEM" ]; then
@@ -127,7 +100,7 @@ else
 fi
 
 # product.img
-echo "-> Check mount/etc for product.img"
+echo "-> Check mount/etc for super_3 (product)"
 if [ -f "$PRODUCT_IMAGE" ]; then
    # Check if product is mounted
    if [ -d "$PRODUCT" ]; then
@@ -143,7 +116,7 @@ else
 fi
 
 # odm.img
-echo "-> Check mount/etc for odm.img"
+echo "-> Check mount/etc for super_5 (odm)"
 if [ -f "$ODM_IMAGE" ]; then
    # Check if odm is mounted
    if [ -d "$ODM" ]; then
@@ -157,39 +130,8 @@ else
    echo " - odm don't exists, be careful!"
 fi
 
-# opproduct.img
-echo "-> Check mount/etc for opproduct.img"
-if [ -f "$OPPRODUCT_IMAGE" ]; then
-   # Check if product is mounted
-   if [ -d "$OPPRODUCT" ]; then
-      if [ -d "$OPPRODUCT/etc/" ]; then
-         echo " - Mount detected in opproduct, force umount!"
-         sudo umount "$OPPRODUCT/"
-      fi
-   fi
-   echo " - Done: opproduct"
-else
-   echo " - opproduct don't exists, be careful!"
-fi
-
-# product.img
-echo "-> Check mount/etc for system_ext.img"
-if [ -f "$SYSTEM_EXT_IMAGE" ]; then
-   echo " - Android 11 detected"
-   # Check if product is mounted
-   if [ -d "$SYSTEM_EXT" ]; then
-      if [ -d "$SYSTEM_EXT/etc/" ]; then
-         echo " - Mount detected in system_ext, force umount!"
-         sudo umount "$SYSTEM_EXT/"
-      fi
-   fi
-   echo " - Done: system_ext"
-else
-   echo " - system_ext don't exists, be careful!"
-fi
-
 echo "-> Starting process!"
-echo " - Mount system"
+echo " - Mount super_2 (system)"
 if [ ! -d "$SYSTEM/" ]; then
    mkdir $SYSTEM
 fi
@@ -201,37 +143,21 @@ if [ ! -d "$SYSTEM_NEW/" ]; then
 fi
 sudo mount -o loop $SYSTEM_NEW_IMAGE $SYSTEM_NEW/
 
-echo " - Mount product"
+echo " - Mount super_3 (product)"
 if [ ! -d "$PRODUCT/" ]; then
    mkdir $PRODUCT
 fi
 sudo mount -o ro $PRODUCT_IMAGE $PRODUCT/
 
 if [ -f "$ODM_IMAGE" ]; then
-   echo " - Mount odm"
+   echo " - Mount super_5 (odm)"
    if [ ! -d "$ODM/" ]; then
       mkdir $ODM
    fi
    sudo mount -o ro $ODM_IMAGE $ODM/
 fi
 
-if [ -f "$OPPRODUCT_IMAGE" ]; then
-   echo " - Mount opproduct"
-   if [ ! -d "$OPPRODUCT/" ]; then
-      mkdir $OPPRODUCT
-   fi
-   sudo mount -o ro $OPPRODUCT_IMAGE $OPPRODUCT/
-fi
-
-if [ -f "$SYSTEM_EXT_IMAGE" ]; then
-   echo " - Mount system_ext"
-   if [ ! -d "$SYSTEM_EXT/" ]; then
-      mkdir $SYSTEM_EXT
-   fi
-   sudo mount -o ro $SYSTEM_EXT_IMAGE $SYSTEM_EXT/
-fi
-
-echo "-> Copy system files to system_new"
+echo "-> Copy super_2 (system) files to system_new"
 cp -v -r -p $SYSTEM/* $SYSTEM_NEW/ > /dev/null 2>&1 && sync
 if [ -d "$SYSTEM_NEW/dev/" ]; then
    cd $LOCALDIR/system_new/system/
@@ -248,7 +174,7 @@ echo "-> Umount system"
 umount $SYSTEM/
 cd $LOCALDIR
 
-echo "-> Copy product files to system_new"
+echo "-> Copy super_3 (product) files to system_new"
 if [ -d "$SYSTEM_NEW/dev/" ]; then
    echo " - Using SAR method"
    cd $LOCALDIR/system_new/
@@ -273,11 +199,11 @@ else
 fi
 cd $LOCALDIR
 
-echo "-> Umount product"
+echo "-> Umount super_3 (product)"
 sudo umount $PRODUCT/
 
 if [ -f "$ODM_IMAGE" ]; then
-   echo "-> Copy odm files to system_new"
+   echo "-> Copy super_5 (odm) files to system_new"
    if [ -d "$SYSTEM_NEW/dev/" ]; then
       echo " - Using SAR method"
       cd $LOCALDIR/system_new/
@@ -308,75 +234,11 @@ if [ -f "$ODM_IMAGE" ]; then
    sudo umount $ODM/
 fi
 
-if [ -f "$OPPRODUCT_IMAGE" ]; then
-echo "-> Copy opproduct files to system_new"
-   if [ -d "$SYSTEM_NEW/dev/" ]; then
-      echo " - Using SAR method"
-      cd $LOCALDIR/system_new/
-      rm -rf oneplus; cd system; rm -rf oneplus
-      mkdir -p oneplus/
-      cp -v -r -p $OPPRODUCT/* oneplus/ > /dev/null 2>&1
-      cd ../
-      echo " - Fix symlink in opproduct"
-      ln -s /system/oneplus/ oneplus
-      sync
-      echo " - Fixed"
-else
-   if [ ! -f "$SYSTEM_NEW/build.prop" ]; then
-      echo "-> Are you sure this is a Android image? Exit"
-      exit 1
-   fi
-   cd $SYSTEM_NEW
-   rm -rf oneplus
-   mkdir oneplus && cd ../
-   cp -v -r -p $OPPRODUCT/* $SYSTEM_NEW/oneplus/ > /dev/null 2>&1 && sync
-   cd $LOCALDIR
-   fi
-fi
-cd $LOCALDIR
-
-if [ -f "$OPPRODUCT_IMAGE" ]; then
-   echo "-> Umount opproduct"
-   sudo umount $OPPRODUCT/
-fi
-
-if [ -f "$SYSTEM_EXT_IMAGE" ]; then
-echo "-> Copy system_ext files to system_new"
-   if [ -d "$SYSTEM_NEW/dev/" ]; then
-      echo " - Using SAR method"
-      cd $LOCALDIR/system_new/
-      rm -rf system_ext; cd system; rm -rf system_ext
-      mkdir -p system_ext/
-      cp -v -r -p $SYSTEM_EXT/* system_ext/ > /dev/null 2>&1
-      cd ../
-      echo " - Fix symlink in system_ext"
-      ln -s /system/system_ext/ system_ext
-      sync
-      echo " - Fixed"
-else
-   if [ ! -f "$SYSTEM_NEW/build.prop" ]; then
-      echo "-> Are you sure this is a Android image? Exit"
-      exit 1
-   fi
-   cd $SYSTEM_NEW
-   rm -rf system_ext
-   mkdir system_ext && cd ../
-   cp -v -r -p $SYSTEM_EXT/* $SYSTEM_NEW/system_ext/ > /dev/null 2>&1 && sync
-   cd $LOCALDIR
-   fi
-fi
-cd $LOCALDIR
-
-if [ -f "$SYSTEM_EXT_IMAGE" ]; then
-   echo "-> Umount system_ext"
-   sudo umount $SYSTEM_EXT/
-fi
-
 echo "-> Umount system_new"
 sudo umount $SYSTEM_NEW/
 
 echo "-> Remove tmp folders and files"
-sudo rm -rf $SYSTEM $SYSTEM_NEW $PRODUCT $SYSTEM_IMAGE $PRODUCT_IMAGE $SYSTEM_EXT $SYSTEM_EXT_IMAGE $OPPRODUCT $OPPRODUCT_IMAGE $ODM $ODM_IMAGE
+sudo rm -rf $SYSTEM $SYSTEM_NEW $PRODUCT $SYSTEM_IMAGE $PRODUCT_IMAGE $ODM $ODM_IMAGE
 
 echo " - Zip system_new.img"
 zip system.img.zip system_new.img
